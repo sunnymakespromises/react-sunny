b10bef266607d707b4cb968e57e8b08e95bd6df5
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2022-11-15 17:34:13 -0500

    removed dropdown 2

---
+++ dropdown/DropdownContext.js
@@ -0,0 +1,16 @@
+import React, { createContext, useContext } from 'react'
+
+const DropdownContext = createContext()
+
+export function DropdownProvider(props) {
+	const { value, children } = props
+	return (
+	   <DropdownContext.Provider value = { value }>
+		    { children }
+	   </DropdownContext.Provider>
+	)
+}
+
+export function useDropdownContext() {
+	return useContext(DropdownContext)
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Container from './components/Container'
-// import Dropdown, { Header, List, Option, Button } from './components/Dropdown'
 import Responsive, { aspectRatio, isLandscape, isPortrait } from './components/Responsive'
 import Text from './components/Text'
 export { Container, Text, Responsive, aspectRatio, isLandscape, isPortrait }
